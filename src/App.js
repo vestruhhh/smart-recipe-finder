22d2813bc3e2b8cb252e2b855e555bcf271c43db
vestruhhh <matthewnicol07@gmail.com>
2024-08-08 00:54:43 -0400

    added nav/footer starter and part of hero section

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import LandingPage from './Pages/LandingPage';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        glitter: '#E2F0F9',
+        powderBlue: '#B0DDE4',
+        greenBlue: '#286FB4',
+        white: '#FFFFFF',
+        fandangoPink: '#DF4C73',
+      },
+    },
+  },
+  plugins: [],
+}
